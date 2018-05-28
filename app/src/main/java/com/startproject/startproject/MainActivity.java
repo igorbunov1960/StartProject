1dd30c15dcea62b8079b6e8204aeb684fa8cf173
Ilya Gorbunov <iliuagorbunov@gmail.com>
2018-05-28 17:58:43 +0300

    lesson 21 activity

--- app/src/main/java/com/startproject/startproject/MainActivity.java
+++ app/src/main/java/com/startproject/startproject/MainActivity.java
@@ -1,77 +1,35 @@
 package com.startproject.startproject;
 
-import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.ContextMenu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.TextView;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
 
-    // константы для ID пунктов меню
-    final int MENU_ALPHA_ID = 1;
-    final int MENU_SCALE_ID = 2;
-    final int MENU_TRANSLATE_ID = 3;
-    final int MENU_ROTATE_ID = 4;
-    final int MENU_COMBO_ID = 5;
+    Button btnActTwo;
 
-    TextView tv;
-
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tv = (TextView) findViewById(R.id.tv);
-        registerForContextMenu(tv);
-        tv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openContextMenu(tv);
-            }
-        });
-
+        btnActTwo = (Button) findViewById(R.id.btnActTwo);
+        btnActTwo.setOnClickListener(this);
     }
 
     @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-        if(v.getId() == R.id.tv) {
-            menu.add(0, MENU_ALPHA_ID, 0, "alpha");
-            menu.add(0, MENU_SCALE_ID, 0, "scale");
-            menu.add(0, MENU_TRANSLATE_ID, 0, "translate");
-            menu.add(0, MENU_ROTATE_ID, 0, "rotate");
-            menu.add(0, MENU_COMBO_ID, 0, "combo");
-        }
-        super.onCreateContextMenu(menu, v, menuInfo);
-    }
-
-
-    @Override
-    public boolean onContextItemSelected(MenuItem item) {
-        Animation anim = null;
-        switch (item.getItemId()) {
-            case MENU_ALPHA_ID:
-                anim = AnimationUtils.loadAnimation(this, R.anim.myalpha);
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnActTwo:
+                Intent intent = new Intent(this, ActivityTwo.class);
+                startActivity(intent);
                 break;
-            case MENU_COMBO_ID:
-                anim = AnimationUtils.loadAnimation(this, R.anim.mycombo);
-                break;
-            case MENU_ROTATE_ID:
-                anim = AnimationUtils.loadAnimation(this, R.anim.myrotate);
-                break;
-            case MENU_SCALE_ID:
-                anim = AnimationUtils.loadAnimation(this, R.anim.myscale);
-                break;
-            case MENU_TRANSLATE_ID:
-                anim = AnimationUtils.loadAnimation(this, R.anim.mytrans);
+            default:
                 break;
         }
-
-        tv.startAnimation(anim);
-        return super.onContextItemSelected(item);
     }
 }

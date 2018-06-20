2824a0ecfd69111dfbd98e4063c187670a91286a
Ilya Gorbunov <iliuagorbunov@gmail.com>
2018-06-20 21:09:05 +0300

    les 38 SQLiteDataBase transactions

--- app/src/main/java/com/startproject/startproject/MainActivity.java
+++ app/src/main/java/com/startproject/startproject/MainActivity.java
@@ -13,85 +13,50 @@ public class MainActivity extends Activity {
 
     final String LOG_TAG = "myLogs";
 
-    // данные для таблицы должностей
-    int[] position_id = { 1, 2, 3, 4 };
-    String[] position_name = { "Директор", "Программер", "Бухгалтер", "Охранник" };
-    int[] position_salary = { 15000, 13000, 10000, 8000 };
-
-    // данные для таблицы людей
-    String[] people_name = { "Иван", "Марья", "Петр", "Антон", "Даша", "Борис", "Костя", "Игорь" };
-    int[] people_posid = { 2, 3, 2, 2, 3, 1, 2, 4 };
+    DBHelper dbh;
+    SQLiteDatabase db;
 
     /** Called when the activity is first created. */
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(LOG_TAG, "--- onCreate Activity ---");
+        dbh = new DBHelper(this);
+        myActions();
+    }
 
-        // Подключаемся к БД
-        DBHelper dbh = new DBHelper(this);
-        SQLiteDatabase db = dbh.getWritableDatabase();
-
-        // Описание курсора
-        Cursor c;
-
-        // выводим в лог данные по должностям
-        Log.d(LOG_TAG, "--- Table position ---");
-        c = db.query("position", null, null, null, null, null, null);
-        logCursor(c);
-        c.close();
-        Log.d(LOG_TAG, "--- ---");
-
-        // выводим в лог данные по людям
-        Log.d(LOG_TAG, "--- Table people ---");
-        c = db.query("people", null, null, null, null, null, null);
-        logCursor(c);
-        c.close();
-        Log.d(LOG_TAG, "--- ---");
-
-        // выводим результат объединения
-        // используем rawQuery
-        Log.d(LOG_TAG, "--- INNER JOIN with rawQuery---");
-        String sqlQuery = "select PL.name as Name, PS.name as Position, salary as Salary "
-                + "from people as PL "
-                + "inner join position as PS "
-                + "on PL.posid = PS.id "
-                + "where salary > ?";
-        c = db.rawQuery(sqlQuery, new String[] {"12000"});
-        logCursor(c);
-        c.close();
-        Log.d(LOG_TAG, "--- ---");
-
-        // выводим результат объединения
-        // используем query
-        Log.d(LOG_TAG, "--- INNER JOIN with query---");
-        String table = "people as PL inner join position as PS on PL.posid = PS.id";
-        String columns[] = { "PL.name as Name", "PS.name as Position", "salary as Salary" };
-        String selection = "salary < ?";
-        String[] selectionArgs = {"12000"};
-        c = db.query(table, columns, selection, selectionArgs, null, null, null);
-        logCursor(c);
-        c.close();
-        Log.d(LOG_TAG, "--- ---");
-
-        // закрываем БД
+    void myActions() {
+        db = dbh.getWritableDatabase();
+        delete(db, "mytable");
+        insert(db, "mytable", "val1");
+        read(db, "mytable");
         dbh.close();
     }
 
-    // вывод в лог данных из курсора
-    void logCursor(Cursor c) {
+    void insert(SQLiteDatabase db, String table, String value) {
+        Log.d(LOG_TAG, "Insert in table " + table + " value = " + value);
+        ContentValues cv = new ContentValues();
+        cv.put("val", value);
+        db.insert(table, null, cv);
+    }
+
+    void read(SQLiteDatabase db, String table) {
+        Log.d(LOG_TAG, "Read table " + table);
+        Cursor c = db.query(table, null, null, null, null, null, null);
         if (c != null) {
+            Log.d(LOG_TAG, "Records count = " + c.getCount());
             if (c.moveToFirst()) {
-                String str;
                 do {
-                    str = "";
-                    for (String cn : c.getColumnNames()) {
-                        str = str.concat(cn + " = " + c.getString(c.getColumnIndex(cn)) + "; ");
-                    }
-                    Log.d(LOG_TAG, str);
+                    Log.d(LOG_TAG, c.getString(c.getColumnIndex("val")));
                 } while (c.moveToNext());
             }
-        } else
-            Log.d(LOG_TAG, "Cursor is null");
+            c.close();
+        }
+    }
+
+    void delete(SQLiteDatabase db, String table) {
+        Log.d(LOG_TAG, "Delete all from table " + table);
+        db.delete(table, null, null);
     }
 
     // класс для работы с БД
@@ -104,37 +69,10 @@ public class MainActivity extends Activity {
         public void onCreate(SQLiteDatabase db) {
             Log.d(LOG_TAG, "--- onCreate database ---");
 
-            ContentValues cv = new ContentValues();
-
-            // создаем таблицу должностей
-            db.execSQL("create table position ("
-                    + "id integer primary key,"
-                    + "name text," + "salary integer"
-                    + ");");
-
-            // заполняем ее
-            for (int i = 0; i < position_id.length; i++) {
-                cv.clear();
-                cv.put("id", position_id[i]);
-                cv.put("name", position_name[i]);
-                cv.put("salary", position_salary[i]);
-                db.insert("position", null, cv);
-            }
-
-            // создаем таблицу людей
-            db.execSQL("create table people ("
+            db.execSQL("create table mytable ("
                     + "id integer primary key autoincrement,"
-                    + "name text,"
-                    + "posid integer"
+                    + "val text"
                     + ");");
-
-            // заполняем ее
-            for (int i = 0; i < people_name.length; i++) {
-                cv.clear();
-                cv.put("name", people_name[i]);
-                cv.put("posid", people_posid[i]);
-                db.insert("people", null, cv);
-            }
         }
 
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
